435ee178e2220ea937825d7e44f086edd5b68047
marino74 <37005560+marino74@users.noreply.github.com>
2018-03-30 21:39:46 -0600

    Add files via upload

---
+++ Chapter01/01_web.js
@@ -0,0 +1,15 @@
+var http = require("http");
+
+function process_request(req, res) {
+    var body = 'Thanks for calling!\n';
+    var content_length = body.length;
+    res.writeHead(200, {
+        'Content-Length': content_length,
+        'Content-Type': 'text/plain'
+    });
+    res.end(body);
+}
+
+var s = http.createServer(process_request);
+s.listen(8089);
+console.log('Server up&running listen in port 8089...');
